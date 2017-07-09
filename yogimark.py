2af76b895f8c7310d00aa0d229990b6fbcb520bd
person <eyal.person.shahar@gmail.com>
2017-07-09 13:40:17 -0700

    bash script added

--- yogimark.py
+++ yogimark.py
@@ -3,14 +3,28 @@ import markovify
 import tweepy
 from credentials import *
 
-# Get raw text as string.
+import argparse
+
+parser = argparse.ArgumentParser(description='YogiMark')
+parser.add_argument("-t", "--notweet", help="disable tweeting", action="store_true")
+args = parser.parse_args()
+
+# create model from corpus
 with open("corpus.txt") as f:
     text = f.read()
-
 text_model = markovify.Text(text, state_size=2)
-phrase = text_model.make_short_sentence(140, tries=1000)
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth)
-api.update_status(phrase)
+model_json = text_model.to_json()
+with open("model.json", 'w') as f:
+  f.write(model_json)
+  
+phrase = text_model.make_short_sentence(140, tries=1000)  
+
+if args.notweet:
+  print "tweeting disabled"
+  print phrase
+else:  
+  auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+  auth.set_access_token(access_token, access_token_secret)
+  api = tweepy.API(auth)
+  api.update_status(phrase)
 
